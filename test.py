f0a2b6d1447617e09f5d08ecebee7b0058cb665e
SuritInfoblox <smusuvathi@infoblox.com>
2024-06-18 11:57:51 +0530

    Test

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ def greet(name):
     """A function that greets the user."""
     print(f"Hello, {name}!")
 
+
+def say_goodbye(name):
+    """A function that says goodbye to the user."""
+    print(f"Goodbye, {name}!")
+
 # Main program
 user_name = input("Enter your name: ")
 greet(user_name)
+
+# Call the say_goodbye function
+say_goodbye(user_name)
+
+
